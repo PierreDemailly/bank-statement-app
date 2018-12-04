ca6ec53bd9422db26a2f831f20fc31cff4d3a6b6
Pierre Demailly <pierredemailly.pro@gmail.com>
2018-12-04 16:20:17 +0100

    fix: classes autoloading quote issue

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ spl_autoload_register(function ()
 {
 	if (file_exists('./model/' . $classname . '.php'))
 	{
-		require ('./model/' . $classname . '.php';
+		require './model/' . $classname . '.php';
 	} 
 	elseif(file_exists('./entities/' . $classname . '.php')) 
 	{
