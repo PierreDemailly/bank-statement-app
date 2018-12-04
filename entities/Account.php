4d8b1997b4bb39e38fd071a0422bf8256f6fa7b5
Pierre Demailly <pierredemailly.pro@gmail.com>
2018-12-04 16:27:05 +0100

    add: account class getters and setters

--- entities/Account.php
+++ entities/Account.php
@@ -1,10 +1,48 @@
 <?php
-
 declare(strict_types = 1);
 
+/**
+ * Class representing a bank account
+ */
 class Account
 {
+  private $id;
+  private $name;
+  private $balance;
+  
+  /**
+   * Get the value of id
+   * @return int
+   */ 
+  public function getId() { return $this->id; }
 
-// Propriétés et méthodes de votre classe ici
+  /**
+   * Set the value of id
+   * @return  self
+   */ 
+  public function setId($id) { $this->id = $id; return $this; }
 
+  /**
+   * Get the value of name
+   * @return string
+   */ 
+  public function getName() { return $this->name; }
+
+  /**
+   * Set the value of name
+   * @return self
+   */ 
+  public function setName($name) { $this->name = $name; return $this; }
+
+  /**
+   * Get the value of balance
+   * @return int
+   */ 
+  public function getBalance() { return $this->balance; }
+
+  /**
+   * Set the value of balance
+   * @return self
+   */ 
+  public function setBalance($balance) { $this->balance = $balance; return $this; }
 }
