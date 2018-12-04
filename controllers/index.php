98e02eb927293e3433808500847ca96b1946f085
Pierre Demailly <pierredemailly.pro@gmail.com>
2018-12-04 20:46:36 +0100

    feat: create an account when submit

--- controllers/index.php
+++ controllers/index.php
@@ -1,3 +1,15 @@
 <?php
+if(isset($_POST['new']))
+{
+  $account_manager = new AccountManager();
+
+  if(in_array($_POST['name'], Account::TYPE_LIST))
+  {
+    if(!$account_manager->accountExist($_POST['name']))
+    {
+      $account_manager->createAccount($_POST['name']);
+    }
+  }
+}
 
 include "./views/indexView.php";
