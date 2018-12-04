3d5102ad4bed8cb135841e66a8c3e31d72c98942
Pierre Demailly <pierredemailly.pro@gmail.com>
2018-12-04 16:26:45 +0100

    fix: autoloading model path rename with models

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
-
 spl_autoload_register(function () 
 {
-	if (file_exists('./model/' . $classname . '.php'))
+	if (file_exists('./models/' . $classname . '.php'))
 	{
-		require './model/' . $classname . '.php';
+		require './models/' . $classname . '.php';
 	} 
-	elseif(file_exists('./entities/' . $classname . '.php')) 
+	elseif (file_exists('./entities/' . $classname . '.php')) 
 	{
 		require './entities/' . $classname . '.php';
 	}
