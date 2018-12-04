308e6f1e5b82bbf9684f4c499991d7cbb602affc
Pierre Demailly <pierredemailly.pro@gmail.com>
2018-12-04 16:02:22 +0100

    refact: autoloading and require controller instead include

--- controllers/index.php
+++ controllers/index.php
@@ -1,21 +1,3 @@
 <?php
 
-// On enregistre notre autoload.
-function chargerClasse($classname)
-{
-    if(file_exists('../model/'. $classname.'.php'))
-    {
-        require '../model/'. $classname.'.php';
-    }
-    else 
-    {
-        require '../entities/' . $classname . '.php';
-    }
-}
-spl_autoload_register('chargerClasse');
-
-
-// Votre code ici 
-
-
 include "../views/indexView.php";
--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-	header("Location: controllers/index.php");
-?>
+
+spl_autoload_register(function () 
+{
+	if (file_exists('./model/' . $classname . '.php'))
+	{
+		require ('./model/' . $classname . '.php';
+	} 
+	elseif(file_exists('./entities/' . $classname . '.php')) 
+	{
+		require './entities/' . $classname . '.php';
+	}
+});
+
+require './controllers/index.php';
